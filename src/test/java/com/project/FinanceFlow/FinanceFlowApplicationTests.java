a3ad9e91dcb822cf4e0d138543b1a57f8e75666d
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2026-06-22 19:28:45 -0300

    Configuração inicial

---
+++ src/test/java/com/project/FinanceFlow/FinanceFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FinanceFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
