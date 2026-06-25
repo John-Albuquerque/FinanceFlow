7974ac7ed60d9e304725bd38e8c03894c96bf4d8
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2026-06-25 16:37:22 -0300

    Padroniza tela de login e cadastro

--- src/main/java/com/project/FinanceFlow/ui/javafx/FxmlView.java
+++ src/main/java/com/project/FinanceFlow/ui/javafx/FxmlView.java
@@ -3,12 +3,12 @@ package com.project.financeflow.ui.javafx;
 public enum FxmlView {
     Login {
         @Override public String getFxmlFile(){ return "/view/auth/login.fxml"; }
-        @Override public String getTitle(){ return "Tela Login"; }
+        @Override public String getTitle(){ return "Finance Flow - Tela Login"; }
     },
 
     Register{
         @Override public String getFxmlFile(){ return "/view/auth/register.fxml"; }
-        @Override public String getTitle(){ return "Tela Cadastro"; }
+        @Override public String getTitle(){ return "Finance Flow - Tela Cadastro"; }
     };
 
     public abstract String getFxmlFile();
