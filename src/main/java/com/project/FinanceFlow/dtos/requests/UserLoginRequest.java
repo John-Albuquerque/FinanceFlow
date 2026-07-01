c76adf5439b74fa9443949149d4cef75f7772652
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2026-07-01 17:32:53 -0300

    adiciona DTO, e padronização de arquivos

---
+++ src/main/java/com/project/FinanceFlow/dtos/requests/UserLoginRequest.java
@@ -0,0 +1,4 @@
+package com.project.financeflow.dtos.requests;
+
+public class UserLoginRequest {
+}
---
+++ src/main/java/com/project/FinanceFlow/dtos/responses/UserCreatedResponse.java
@@ -0,0 +1,4 @@
+package com.project.financeflow.dtos.responses;
+
+public class UserCreatedResponse {
+}
